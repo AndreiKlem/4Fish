31fbf08c4c30920d3cba532d8ccebb2815e9ac2b
AndreiKlem <agrngml@gmail.com>
2020-04-28 15:57:33 +0300

    Build 0.1.0

---
+++ app/src/main/java/com/example/a4fish/AddNewEventActivity.java
@@ -0,0 +1,14 @@
+package com.example.a4fish;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddNewEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_event);
+    }
+}
